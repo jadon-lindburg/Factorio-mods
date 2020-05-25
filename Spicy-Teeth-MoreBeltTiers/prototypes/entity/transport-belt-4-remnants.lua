8964a88babbe7a4cf51591529c6489c309e4b7c1
Jadon Lindburg <lindburg.jadon@gmail.com>
2020-05-25 16:21:05 -0600

    Implemented tier 4 belts

---
+++ Spicy-Teeth-MoreBeltTiers/prototypes/entity/transport-belt-4-remnants.lua
@@ -0,0 +1,45 @@
+data:extend(
+{
+    {
+        type = "corpse",
+        name = "transport-belt-4-remnants",
+        icon = "__spicy-teeth-more-belt-tiers__/graphics/icons/transport-belt-4.png",
+        icon_size = 32,
+        flags = {"placeable-neutral", "not-on-map"},
+        selection_box = {{-0.5, -0.5}, {0.5, 0.5}},
+        tile_width = 1,
+        tile_height = 1,
+        selectable_in_game = false,
+        subgroup = "remnants",
+        order="d[remnants]-a[generic]-a[small]",
+        time_before_removed = 60 * 60 * 15, -- 15 minutes
+        final_render_layer = "remnants",
+        remove_on_tile_placement = false,
+        animation =  make_rotated_animation_variations_from_sheet (2,
+        {
+            filename = "__spicy-teeth-more-belt-tiers__/graphics/entity/transport-belt-4/remnants/transport-belt-4-remnants.png",
+            line_length = 1,
+            width = 54,
+            height = 52,
+            frame_count = 1,
+            variation_count = 1,
+            axially_symmetrical = false,
+            direction_count = 4,
+            shift = util.by_pixel(1, 0),
+            hr_version =
+            {
+                filename = "__spicy-teeth-more-belt-tiers__/graphics/entity/transport-belt-4/remnants/hr-transport-belt-4-remnants.png",
+                line_length = 1,
+                width = 106,
+                height = 102,
+                frame_count = 1,
+                variation_count = 1,
+                axially_symmetrical = false,
+                direction_count = 4,
+                shift = util.by_pixel(1, -0.5),
+                scale = 0.5,
+            },
+        })
+    },
+}
+)
---
+++ Spicy-Teeth-MoreBeltTiers/prototypes/item/splitter-4.lua
@@ -0,0 +1,14 @@
+data:extend(
+{
+    {
+        type = "item",
+        name = "splitter-4",
+        icon = "__spicy-teeth-more-belt-tiers__/graphics/icons/splitter-4.png",
+        icon_size = 32,
+        subgroup = "belt",
+        order = "c[splitter]-d[splitter-4]",
+        place_result = "splitter-4",
+        stack_size = 50
+    },
+}
+)
