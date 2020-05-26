776cdd7902d1236fba496d118384522fcc8c70aa
Jadon Lindburg <lindburg.jadon@gmail.com>
2020-05-26 17:12:25 -0600

    Forgot to increase max underground belt distance

--- source files/SpicyTeeth-MoreBeltTiers/prototypes/entity/underground-belt-5.lua
+++ source files/SpicyTeeth-MoreBeltTiers/prototypes/entity/underground-belt-5.lua
@@ -11,7 +11,7 @@ data:extend(
         minable = {mining_time = 0.1, result = "underground-belt-5"},
         max_health = 190,
         corpse = "underground-belt-5-remnants",
-        max_distance = 11,
+        max_distance = 13,
         underground_sprite =
         {
             filename = "__core__/graphics/arrows/underground-lines.png",
