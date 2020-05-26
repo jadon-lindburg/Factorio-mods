a260faa7e8d097a5f27187363a5a7f6d6cde9b82
Jadon Lindburg <lindburg.jadon@gmail.com>
2020-05-26 16:59:32 -0600

    Didn't update entire filepaths

--- source files/SpicyTeeth-MoreBeltTiers/prototypes/entity/transport-belt-5-pictures.lua
+++ source files/SpicyTeeth-MoreBeltTiers/prototypes/entity/transport-belt-5-pictures.lua
@@ -2,7 +2,7 @@ tier_5_belt_animation_set =
 {
     animation_set =
     {
-        filename = "__spicy-teeth-more-belt-tiers__/graphics/entity/transport-belt-4/transport-belt-5.png",
+        filename = "__spicy-teeth-more-belt-tiers__/graphics/entity/transport-belt-5/transport-belt-5.png",
         priority = "extra-high",
         width = 64,
         height = 64,
@@ -10,7 +10,7 @@ tier_5_belt_animation_set =
         direction_count = 20,
         hr_version =
         {
-            filename = "__spicy-teeth-more-belt-tiers__/graphics/entity/transport-belt-4/hr-transport-belt-5.png",
+            filename = "__spicy-teeth-more-belt-tiers__/graphics/entity/transport-belt-5/hr-transport-belt-5.png",
             priority = "extra-high",
             width = 128,
             height = 128,
