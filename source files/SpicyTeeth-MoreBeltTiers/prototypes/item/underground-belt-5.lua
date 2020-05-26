7ca109e9da7ece0368b8e26c716995392971a722
Jadon Lindburg <lindburg.jadon@gmail.com>
2020-05-26 16:40:02 -0600

    Added item files

---
+++ source files/SpicyTeeth-MoreBeltTiers/prototypes/item/underground-belt-5.lua
@@ -0,0 +1,14 @@
+data:extend(
+{
+    {
+        type = "item",
+        name = "underground-belt-5",
+        icon = "__spicy-teeth-more-belt-tiers__/graphics/icons/underground-belt-5.png",
+        icon_size = 32,
+        subgroup = "belt",
+        order = "b[underground-belt]-e[underground-belt-5]",
+        place_result = "underground-belt-5",
+        stack_size = 50
+    },
+}
+)
