618d679b912e94c5ca497d843ebd159cf4618720
Jadon Lindburg <lindburg.jadon@gmail.com>
2020-05-26 19:58:34 -0600

    Added item files for tier 6

---
+++ source files/SpicyTeeth-MoreBeltTiers/prototypes/item/underground-belt-6.lua
@@ -0,0 +1,14 @@
+data:extend(
+{
+  {
+    type = "item",
+    name = "underground-belt-6",
+    icon = "__spicy-teeth-more-belt-tiers__/graphics/icons/underground-belt-6.png",
+    icon_size = 32,
+    subgroup = "belt",
+    order = "b[underground-belt]-f[underground-belt-6]",
+    place_result = "underground-belt-6",
+    stack_size = 50
+  },
+}
+)
