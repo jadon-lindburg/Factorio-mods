b322b60fd5fb4dfb77ca07de7d47d2145c5506ac
Jadon Lindburg <lindburg.jadon@gmail.com>
2020-05-26 20:02:39 -0600

    Added recipes for tier 6

---
+++ source files/SpicyTeeth-MoreBeltTiers/prototypes/recipe/transport-belt-6.lua
@@ -0,0 +1,31 @@
+data:extend(
+{
+	{
+		type = "recipe",
+		name = "transport-belt-6",
+		category = "crafting-with-fluid",
+		normal =
+		{
+			enabled = false,	  
+			ingredients =
+			{
+				{"steel-gear-wheel", 10},
+				{"transport-belt-4", 1},
+				{type="fluid", name="refined-lubricant", amount=50}
+			},
+			result = "transport-belt-6"
+		},
+		expensive =
+		{
+			enabled = false,	  
+			ingredients =
+			{
+				{"steel-gear-wheel", 20},
+				{"transport-belt-5", 1},
+				{type="fluid", name="refined-lubricant", amount=50}
+			},	  
+			result = "transport-belt-6"
+		}	
+	}
+}
+)
