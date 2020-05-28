565fa40de2a1eec3ef4733fa23e2cf049b0a1258
Jadon Lindburg <lindburg.jadon@gmail.com>
2020-05-28 16:48:41 -0600

    Added item and recipe files for tier 7

---
+++ source files/SpicyTeeth-MoreBeltTiers/prototypes/item/splitter-7.lua
@@ -0,0 +1,14 @@
+data:extend(
+{
+  {
+    type = "item",
+    name = "splitter-7",
+    icon = "__spicy-teeth-more-belt-tiers__/graphics/icons/splitter-7.png",
+    icon_size = 32,
+    subgroup = "belt",
+    order = "c[splitter]-g[splitter-7]",
+    place_result = "splitter-7",
+    stack_size = 50
+  },
+}
+)
---
+++ source files/SpicyTeeth-MoreBeltTiers/prototypes/recipe/transport-belt-7.lua
@@ -0,0 +1,31 @@
+data:extend(
+{
+	{
+		type = "recipe",
+		name = "transport-belt-7",
+		category = "crafting-with-fluid",
+		normal =
+		{
+			enabled = false,	  
+			ingredients =
+			{
+				{"steel-gear-wheel", 15},
+				{"transport-belt-6", 1},
+				{type="fluid", name="aerated-lubricant", amount=50}
+			},
+			result = "transport-belt-7"
+		},
+		expensive =
+		{
+			enabled = false,	  
+			ingredients =
+			{
+				{"steel-gear-wheel", 30},
+				{"transport-belt-6", 1},
+				{type="fluid", name="aerated-lubricant", amount=50}
+			},	  
+			result = "transport-belt-7"
+		}	
+	}
+}
+)
