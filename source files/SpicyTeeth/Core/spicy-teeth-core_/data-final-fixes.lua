574c6707f0cc89b54e3e142131a227a66350f588
Jadon Lindburg <lindburg.jadon@gmail.com>
2020-08-09 18:58:33 -0600

    Changed how prototypes are changed in data-final-fixes to better method

--- source files/SpicyTeeth/Core/spicy-teeth-core_/data-final-fixes.lua
+++ source files/SpicyTeeth/Core/spicy-teeth-core_/data-final-fixes.lua
@@ -1,1 +1,8 @@
-data.raw["resource"]["stone"].map_color = {0.483, 0.428, 0.299}
+-- store prototypes to change locally so they don't have to be retrieved more than once
+local resource_stone = data.raw["resource"]["stone"]
+-- /store prototypes to change
+
+
+
+-- change map color of stone
+resource_stone.map_color = {0.483, 0.428, 0.299}
